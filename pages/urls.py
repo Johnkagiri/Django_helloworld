4a4c0fd7871090b969d7f42a3949e378773538f3
johnkagiri <kagirijohn124@gmail.com>
2024-11-06 12:54:53 +0300

    added post model

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, PostHomePageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about", AboutPageView.as_view(), name="about"),
+    path("post", PostHomePageView.as_view(), name="posthome"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Post
 
 # Create your views here.
 
@@ -11,4 +12,8 @@ class HomePageView(TemplateView):
     template_name="home.html"
 
 class AboutPageView(TemplateView):
-    template_name= "about.html"    
+    template_name= "about.html" 
+
+class PostHomePageView(ListView):
+    model = Post
+    template_name = "posthome.html"
