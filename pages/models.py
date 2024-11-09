1655f2cd224f1d4957cea96e6ab1cb8d1ac5d5c4
johnkagiri <kagirijohn124@gmail.com>
2024-11-09 18:07:25 +0300

    blog app

--- pages/models.py
+++ pages/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
-    text= models.TextField()
-
-    def __str__(self):
-        return self.text[:50]
+    title= models.CharField(max_length=200, default="default value")
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE, null=True)
+    body = models.TextField(null=True)
     
+    def __str__(self):
+        return self.title[:50]
+    
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"pk": self.pk})
--- pages/tests.py
+++ pages/tests.py
@@ -1,5 +1,6 @@
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, TestCase
 from django.urls import reverse
+from .models import Post
 
 # Create your tests here.
 class HomePageTests(SimpleTestCase):
@@ -18,5 +19,14 @@ class AboutpageTests(SimpleTestCase):
 
     def test_url_available_by_name(self): 
         response = self.client.get(reverse("about"))
-        self.assertEqual(response.status_code, 200)    
+        self.assertEqual(response.status_code, 200)
+
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post=Post.objects.create(text="This is a test")
+
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "This is a test")    
+                
 
